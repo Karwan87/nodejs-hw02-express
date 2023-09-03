ebca05cbc47b3c4fcdbd2c9398fc84817e606b2e
Karwan87 <damianuszynski@gmail.com>
2023-09-03 21:38:49 +0200

    Update contacts.js
    commented JSON
    created a new one to use with MongoDB

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,56 +1,132 @@
 const express = require("express");
-const contactsModel = require("../../models/contacts");
-const Joi = require("joi");
+const contactsRouter = express.Router();
+const Contact = require("../../models/contactModel");
+const { listContacts } = require("../../models/contacts");
 
-const router = express.Router();
+// const Joi = require("joi");
+// working with JSON file
 
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
+// const contactSchema = Joi.object({
+//   name: Joi.string().required(),
+//   email: Joi.string().email().required(),
+//   phone: Joi.string().required(),
+// });
+// router.get("/", async (req, res, next) => {
+//   try {
+//     const contactsList = await contactsModel.listContacts();
+//     res.status(200).json(contactsList);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.get("/", async (req, res, next) => {
+// router.get("/:contactId", async (req, res, next) => {
+//   const contactId = req.params.contactId;
+//   try {
+//     const contact = await contactsModel.getContactById(contactId);
+//     if (contact) {
+//       res.status(200).json(contact);
+//     } else {
+//       res.status(400).json({ message: "Contact not found" });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+// router.post("/", async (req, res, next) => {
+//   const { error } = contactSchema.validate(req.body);
+//   if (error) {
+//     return res.status(400).json({ message: error.details[0].message });
+//   }
+
+//   try {
+//     const newContact = await contactsModel.addContact(req.body);
+//     res.status(201).json(newContact);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+// router.delete("/:contactId", async (req, res, next) => {
+//   const contactId = req.params.contactId;
+//   try {
+//     const result = await contactsModel.removeContact(contactId);
+//     if (result) {
+//       res.status(200).json({ message: "Contact deleted" });
+//     } else {
+//       res.status(404).json({ message: "Not found" });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+// router.put("/:contactId", async (req, res, next) => {
+//   const contactId = req.params.contactId;
+//   const updatedData = req.body;
+
+//   if (!updatedData || Object.keys(updatedData).length === 0) {
+//     return res.status(400).json({ message: "missing fields" });
+//   }
+
+//   try {
+//     const updatedContact = await contactsModel.updateContact(
+//       contactId,
+//       updatedData
+//     );
+
+//     if (updatedContact) {
+//       res.status(200).json(updatedContact);
+//     } else {
+//       res.status(404).json({ message: "Not found" });
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+//  working with mongoDB
+
+// Pobierz wszystkie kontakty
+contactsRouter.get("/", async (req, res) => {
   try {
-    const contactsList = await contactsModel.listContacts();
-    res.status(200).json(contactsList);
+    const contacts = await listContacts();
+    res.json(contacts);
   } catch (error) {
-    next(error);
+    console.error("Error fetching contacts:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
-router.get("/:contactId", async (req, res, next) => {
+// Pobierz konkretny kontakt po ID
+contactsRouter.get("/:contactId", async (req, res, next) => {
   const contactId = req.params.contactId;
   try {
-    const contact = await contactsModel.getContactById(contactId);
+    const contact = await Contact.findById(contactId);
     if (contact) {
       res.status(200).json(contact);
     } else {
-      res.status(400).json({ message: "Contact not found" });
+      res.status(404).json({ message: "Contact not found" });
     }
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/", async (req, res, next) => {
-  const { error } = contactSchema.validate(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
-
+// Dodaj nowy kontakt
+contactsRouter.post("/", async (req, res, next) => {
+  const newContactData = req.body;
   try {
-    const newContact = await contactsModel.addContact(req.body);
+    const newContact = await Contact.create(newContactData);
     res.status(201).json(newContact);
   } catch (error) {
     next(error);
   }
 });
 
-router.delete("/:contactId", async (req, res, next) => {
+// Usuń kontakt po ID
+contactsRouter.delete("/:contactId", async (req, res, next) => {
   const contactId = req.params.contactId;
   try {
-    const result = await contactsModel.removeContact(contactId);
+    const result = await Contact.findByIdAndDelete(contactId);
     if (result) {
       res.status(200).json({ message: "Contact deleted" });
     } else {
@@ -60,8 +136,7 @@ router.delete("/:contactId", async (req, res, next) => {
     next(error);
   }
 });
-
-router.put("/:contactId", async (req, res, next) => {
+contactsRouter.put("/:contactId", async (req, res, next) => {
   const contactId = req.params.contactId;
   const updatedData = req.body;
 
@@ -70,9 +145,34 @@ router.put("/:contactId", async (req, res, next) => {
   }
 
   try {
-    const updatedContact = await contactsModel.updateContact(
+    const updatedContact = await Contact.findByIdAndUpdate(
       contactId,
-      updatedData
+      updatedData,
+      { new: true }
+    );
+
+    if (updatedContact) {
+      res.status(200).json(updatedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+contactsRouter.patch("/:contactId/favorite", async (req, res, next) => {
+  const contactId = req.params.contactId;
+  const { favorite } = req.body;
+
+  if (favorite === undefined) {
+    return res.status(400).json({ message: "missing field favorite" });
+  }
+
+  try {
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      { favorite },
+      { new: true }
     );
 
     if (updatedContact) {
@@ -85,4 +185,4 @@ router.put("/:contactId", async (req, res, next) => {
   }
 });
 
-module.exports = router;
+module.exports = contactsRouter;
