11758c73834eb49bbd3a8aace8201df41f0b4841
Karwan87 <damianuszynski@gmail.com>
2023-09-12 23:34:09 +0200

    Update authMiddleware.js
    token checking

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -3,24 +3,16 @@ const { Unauthorized } = require("http-errors");
 const config = require("../config");
 
 const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  console.log("Received token:", token);
   try {
-    // Get token from Authorization header
-    const token = req.headers.authorization?.split(" ")[1];
-
     if (!token) {
       throw new Unauthorized("Not authorized");
     }
-
-    // Verify token using the secret key from config
-    const decoded = jwt.verify(token, config.secretKey);
-
-    // Save user's data in req.user
+    const decoded = jwt.verify(token, config.jwtSecret);
     req.user = decoded;
-
-    // Continue to the next middleware
     next();
   } catch (error) {
-    // If authorization failed, return Unauthorized error
     next(new Unauthorized("Not authorized"));
   }
 };
