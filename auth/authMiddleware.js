0467608a58caaa0f54a64b7108293b2eed2aad01
Karwan87 <damianuszynski@gmail.com>
2023-09-17 22:46:28 +0200

    Update authMiddleware.js

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken");
 const { Unauthorized } = require("http-errors");
 const config = require("../config");
+const User = require("../models/userModel");
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   console.log("Received token:", token);
   try {
@@ -10,7 +11,11 @@ const authMiddleware = (req, res, next) => {
       throw new Unauthorized("Not authorized");
     }
     const decoded = jwt.verify(token, config.jwtSecret);
-    req.user = decoded;
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      throw new Unauthorized("Not authorized");
+    }
+    req.user = user;
     next();
   } catch (error) {
     next(new Unauthorized("Not authorized"));
